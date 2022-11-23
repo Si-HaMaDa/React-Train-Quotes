0032940249c0af67d8aede3a2091c128a2223068
Si HaMaDa <nrookman_27@yahoo.com>
2022-11-23 18:32:40 +0200

    sending Comments data

--- src/components/comments/NewCommentForm.jsx
+++ src/components/comments/NewCommentForm.jsx
@@ -1,23 +1,46 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
 
 import classes from "./NewCommentForm.module.css";
 
 const NewCommentForm = (props) => {
     const commentTextRef = useRef();
 
+    const { sendRequest, status, error } = useHttp(addComment);
+
+    const { onAddedComment } = props;
+
+    useEffect(() => {
+        if (status === "completed" && !error) {
+            onAddedComment();
+        }
+    }, [status, error, onAddedComment]);
+
     const submitFormHandler = (event) => {
         event.preventDefault();
 
-        // optional: Could validate here
+        const enteredText = commentTextRef.current.value;
 
-        // send comment to server
+        if (!enteredText) return;
+
+        sendRequest({
+            commentData: { text: enteredText },
+            quoteId: props.quoteId,
+        });
     };
 
     return (
         <form className={classes.form} onSubmit={submitFormHandler}>
             <div className={classes.control} onSubmit={submitFormHandler}>
                 <label htmlFor="comment">Your Comment</label>
-                <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
+                <textarea
+                    id="comment"
+                    name="comment"
+                    rows="5"
+                    ref={commentTextRef}
+                    required
+                ></textarea>
             </div>
             <div className={classes.actions}>
                 <button className="btn">Add Comment</button>
